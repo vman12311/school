149bb7cee352cfd13fb61e62f7ecc104b096815e
vman12311 <54085842+vman12311@users.noreply.github.com>
2020-11-12 03:49:47 -0500

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,20 +1,38 @@
-#include <linux/init.h> // Macros used to mark up functions e.g., __init __exit
-#include <linux/module.h>// Core header for loading LKMs into the kernel
-#include <linux/kernel.h> // Contains types, macros, functions for the kernel
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched/signal.h>
+#include <linux/moduleparam.h>
+#include <linux/unistd.h>
+#include "/usr/src/linux-headers-5.4.0-52-generic/include/linux/string.h"
 
 MODULE_LICENSE("GPL");///< The license type -- this affects runtime behavior
-MODULE_AUTHOR(“Miryala ");    ///< The author -- visible when you use modinfo
+MODULE_AUTHOR("Miryala ");    ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("PID check");///< The description -- see modinfo
 MODULE_VERSION("0.1");///< The version of the module
 
-static char *name = "world"; ///< An example LKM argument -- default value is "world"
-module_param(name, charp, S_IRUGO); 
-///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+static char *name = "world";
+module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
 ///< parameter descript
 
-static int __init hello_init(void){
- printk(KERN_INFO “TDR: Hello %s from the Test LKM!\n", name);
+static int __init findtask_init(void){
+ struct task_struct* task_list;
+ for_each_process(task_list)
+ {
+     pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
+ }
+ 
+ string Task = strcmp(comm, name);
+ if (Task == 0)
+ {
+    printk(KERN_INFO "Found Process", name, pid);
+ }
+ else
+ {
+     printk(KERN_INFO "Process Not Found", name);
+ }
+ 
  return 0;
 }
 
@@ -22,12 +40,12 @@ static int __init hello_init(void){
 *Similar to the initialization function, it is static. The __exit macro notifies if this
 *code is used for a built-in driver (not a LKM) that this function is not required.
 */
-static void __exit hello_exit(void){
- printk(KERN_INFO “TDR: Goodbye %s from the Test LKM!\n", name);
+static void __exit findtask_exit(void){
+ printk(KERN_INFO "TDR: Goodbye %s from the Test LKM!\n", name);
 }
 
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, which identify the initialization function at insertion time and the cleanup function (as listed above)
 */
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(findtask_init);
+module_exit(findtask_exit);
