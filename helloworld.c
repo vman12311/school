7678d385cf58d1df9cae211f793e01802496bd10
vman12311 <54085842+vman12311@users.noreply.github.com>
2020-11-12 05:39:37 -0500

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,25 +2,35 @@
 #include <linux/kernel.h>       // KERN_INFO
 #include <linux/sched/signal.h>
 
-static char *name = "processname";
-static int Task;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Vikyat ");
+MODULE_DESCRIPTION("A simple Linux driver.");
+MODULE_VERSION("0.1");
+
+static char *name;
 module_param(name, charp, S_IRUGO);
 MODULE_PARM_DESC(name, "The name requested");
 
 void procs_info_print(void)
 {
+        int Task = 1;
+        int Num = 0;
         struct task_struct* task_list;
         size_t process_counter = 0;
         for_each_process(task_list) {
-                pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
                 ++process_counter;
-        Task = strcmp(task_list->comm, name);
+        int Comp = strcmp(task_list->comm, name);
+        if (Comp == 0)
+        {
+            Task = Comp;
+            Num = task_list->pid;
+        }
 
         }
     
         if (Task == 0)
         {
-            printk(KERN_INFO "Found Process", name, task_list->pid);
+            printk(KERN_INFO "Found Process", name, Num);
         }
         else
         {
